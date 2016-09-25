4512f8f00283c83c132d4dc2840ebe5426a24ff8
Huayu Yang <yhygoo@gmail.com>
2016-09-24 23:53:58 -0700

    server protobuf

--- server/Src/LuaInterface/lua_protobufLoader.c
+++ server/Src/LuaInterface/lua_protobufLoader.c
@@ -9,25 +9,21 @@
 
 static int unpack (lua_State *L) {
 	printf("unpack function called\n");
-	printf("%d", lua_gettop(L));
+	printf("%d\n", lua_gettop(L));
 	//struct _Owlies__Core__ChangeEvents__Item deserializeMessage(void *buf, unsigned len)
 	void *buf = lua_touserdata(L, 1);
-	printf("1\n");
-	lua_pop(L, 1);
-	printf("2\n");
-	int sz = lua_tointeger(L,1);
-	lua_pop(L, 1);
+	int sz = lua_tointeger(L,2);
 	printf("%d\n", sz);
 
-	//struct _Owlies__Core__ChangeEvents__Item item = deserializeMessage(buf, sz);
-	lua_getglobal(L, "deserializeMessage");
-	lua_pushlightuserdata(L ,buf);
-	lua_pushinteger(L, sz);
-	lua_call(L, 2, 1);
-	lua_setglobal(L, "item");
-	struct _Owlies__Core__ChangeEvents__Item *item = lua_touserdata(L, 1);
-	lua_pop(L, 1);
-	printf("item name: %s\n", (*item).name);
+	struct _Owlies__Core__ChangeEvents__Item item = deserializeMessage(buf, sz);
+	// lua_getglobal(L, "deserializeMessage");
+	// lua_pushlightuserdata(L ,buf);
+	// lua_pushinteger(L, sz);
+	// lua_call(L, 2, 1);
+	// lua_setglobal(L, "item");
+	// struct _Owlies__Core__ChangeEvents__Item *item = lua_touserdata(L, 1);
+	// lua_pop(L, 1);
+	printf("item name: %s\n", item.name);
 	return 0;
 }
 
