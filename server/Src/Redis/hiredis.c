21541e5659b31b1e93f8435e415ca9fb53ea4f15
baichuan <owlies.baichuan@gmail.com>
2017-07-21 23:46:40 -0700

    Add retry mechanism in hiredis.c (Do not merge it to master branch before unit test is done)

---
+++ server/Src/Redis/hiredis.c
@@ -0,0 +1,167 @@
+#include <lua.h>
+#include <lauxlib.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include <hiredis/hiredis.h>
+#include <hiredis/async.h>
+
+#define MAX_RETRY_TIMES 5
+
+char * REDIS_SET_BYTE_STRING_ERROR = "Fail to send key-value pair to Redis due to connection failure";
+char * REDIS_GET_BYTE_STRING_ERROR = "Fail to get key-value pair from Redis due to connection failure";
+
+typedef struct clientconfig {
+    char *ip;
+    int port;
+    struct timeval timeout;
+}config;
+
+static int init(lua_State *L) {
+    config.ip = lua_tostring(L, 1);
+    config.port = lua_tointeger(L, 2);
+    config.timeout = { 1, 500000 };
+    printf("redis ip: %s:%d\n", ip, port);
+
+    redisContext *context;
+    int retry_time = 0;
+
+    while(retry_time < MAX_RETRY_TIMES){
+        ++ retry_time;
+        if(connect(context) != -1 && ping(context) != -1)
+            break;
+    }
+
+    if(retry_time >= MAX_RETRY_TIMES) {
+        printf("Already tried many times to connect to Redis but all failed.");
+        if(context)
+            redisFree(context);
+        exit(1);
+    }
+    lua_pushlightuserdata(L, context);
+    return 1;
+}
+
+int connect(redisContext *c) {
+    c = redisConnectWithTimeout(config.ip, config.port, config.timeout);
+    if (c == NULL || c->err) {
+        if (c) {
+            printf("Connection error: %s\n", c->errstr);
+            redisFree(c);
+        } else {
+            printf("Connection error: can't allocate redis context\n");
+        }
+        return -1;
+    }
+
+    return 1;
+}
+
+static int disconnect(lua_State *L) {
+    redisContext *context = lua_touserdata(L, 1);
+    redisFree(context);
+    return 0;
+}
+
+static int setByteString(lua_State *L) {
+    redisContext *context = lua_touserdata(L, 1);
+    const char *key = lua_tostring(L, 2);
+    const char *value = lua_tostring(L, 3);
+    int sz = lua_tointeger(L, 4);
+
+    int retry_time = 0;
+
+    //If ping redis fails, rebuild the connection and ensure that new connection is working/pinged.
+    if(ping(context) == -1) {
+        while(retry_time < MAX_RETRY_TIMES){
+            ++ retry_time;
+            if(connect(context) != -1 && ping(context) != -1)
+                break;
+        }
+    }
+    
+    if(retry_time >= MAX_RETRY_TIMES) {
+        printf("Already tried many times to connect to Redis but all failed.");
+        if(context)
+            redisFree(context);
+        lua_pushlstring(L, REDIS_SET_BYTE_STRING_ERROR, strlen(*REDIS_SET_BYTE_STRING_ERROR));
+        return 1;
+    }
+
+ 
+    redisReply *reply;
+    reply = redisCommand(context, "SET %s %b", key, value, (size_t)sz);
+
+    //Note: reply->str can contain error information
+    lua_pushlstring(L, reply->str, reply->len);
+
+    freeReplyObject(reply);
+    reply = NULL;
+
+    return 1;
+}
+
+static int getByteString(lua_State *L) {
+    redisContext *context = lua_touserdata(L, 1);
+    const char *key = lua_tostring(L, 2);
+
+    //If ping redis fails, rebuild the connection and ensure that new connection is working/pinged.
+    if(ping(context) == -1) {
+        while(retry_time < MAX_RETRY_TIMES){
+            ++ retry_time;
+            if(connect(context) != -1 && ping(context) != -1)
+                break;
+        }
+    }
+
+    if(retry_time >= MAX_RETRY_TIMES) {
+        printf("Already tried many times to connect to Redis but all failed.");
+        if(context)
+            redisFree(context);
+        lua_pushlstring(L, REDIS_GET_BYTE_STRING_ERROR, strlen(*REDIS_GET_BYTE_STRING_ERROR));
+        return 1;
+    }
+
+    redisReply *reply;
+    reply = redisCommand(context, "GET %s", key);
+
+    //Note: reply->str can contain error information
+    lua_pushlstring(L, reply->str, reply->len);
+
+    freeReplyObject(reply);
+    reply = NULL;
+    return 1;
+}
+
+int ping(redisContext *context) {
+    if(context == NULL)
+        return -1;
+
+    redisReply *reply;
+    reply = redisCommand(context, "PING");
+    int res = -1;
+    if (strcmp(reply->str, "PONG") == 0) {
+        res = 0;
+    }
+
+    freeReplyObject(reply);
+    return res;
+}
+
+int luaopen_hiredis(lua_State *L) {
+	luaL_checkversion(L);
+
+	luaL_Reg l[] = {
+        {"redisInit", init},
+        {"redisDisconnect", disconnect},
+        {"redisSetByteString", setByteString},
+        {"redisGetByteString", getByteString},
+		{ NULL, NULL },
+	};
+
+	luaL_newlib(L, l);
+
+	return 1;
+}
