7a5a1e356233ba5f5e1aa45ba660e463de726316
Huayu Yang <owlies.hwayu@gmail.com>
2017-08-01 21:18:15 -0700

    user_login sql

---
+++ database/user_login.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `user_login` (
+  `user_id` varchar(255) NOT NULL DEFAULT '',
+  `user_account` varchar(255) DEFAULT NULL,
+  `device_identifier` varchar(255) DEFAULT NULL,
+  `client_version` varchar(255) DEFAULT NULL,
+  `client_app_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
